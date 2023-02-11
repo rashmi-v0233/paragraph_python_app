d8a2166fd9c6ef6983c9851b9f4529fc837fdce4
rashmi-v0233 <107745797+rashmi-v0233@users.noreply.github.com>
2023-02-11 12:15:39 -0500

    A very simple Py app to test the full docker flow
    This is just an initial file to save the effort of typing the whole code given in the problem.
    I will add the code once I test the full docker flow.

---
+++ main.py
@@ -0,0 +1,7 @@
+# This is a very simple Python script I am writing to put as first version of my docker image of this app.
+# If time permits, I will type the code given in the problem.
+def show_lines(n):
+  print("You have entered: ", n)
+  
+if __name__ == "__main__":
+  def show_lines(4)
